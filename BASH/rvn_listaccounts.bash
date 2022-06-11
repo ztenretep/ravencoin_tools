d63c994e612c34079193c6ef7d7d43af603451d9
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-06-11 11:37:42 +0200

    Create rvn_listaccounts.bash

---
+++ BASH/rvn_listaccounts.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+#
+# Description: Lists accounts.
+#
+# rpc-user and rpc-password have to be set in the raven.conf.
+
+# Set user and password.
+#USER="<rpc-user>"
+#PASSWORD="<rpc-password>"
+
+# Set service url.
+URL="http://127.0.0.1:8766/"
+
+# Set service.
+SERVICE="listaccounts"
+
+# Set the request header.
+HEADER="content-type: text/plain;"
+
+# Set the request data.
+DATA="{
+       \"jsonrpc\": \"1.0\", \"id\":\"curlreq\", \"method\": \"${SERVICE}\", \"params\": []
+      }"
+
+# Get response from interface.
+response=$(curl -s --user "${USER}":"${PASSWORD}" --data-binary "${DATA}" -H "${HEADER}" "${URL}")
+
+# Print response to screen.
+echo -e "${response}" | jq
+
+# Exit script without error.
+exit 0
